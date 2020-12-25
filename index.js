d9e7135cd0e43fe7cffe2ac6d681bf6995f89906
Ismail-Jaman <mdismail9n9@gmail.com>
2020-12-26 02:48:35 +0600

    Created The Basic Sturcture

---
+++ index.js
@@ -0,0 +1,15 @@
+const tilesArray = ["tiles1","tiles2","tiles3","tiles4","tiles5",
+                   "tiles6","tiles7","tiles8","tiles9","tiles10"];
+
+const colorArray = ["red", "blue", "purple", "yellow", "black"];
+
+
+redArray = [];
+
+blueArray = [];
+
+purpleArray = [];
+
+yellowArray = [];
+
+blackArray = [];
